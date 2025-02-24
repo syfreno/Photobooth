bd623d09be8164135390f4606455f4edfc86387a
Agnes <agnesye153@gmail.com>
2025-02-24 15:14:42 -0500

    hamburger

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";  
 import Home from "./components/Home";
 import Welcome from "./components/Welcome";
@@ -11,6 +11,11 @@ import Contact from "./components/Contact";
 
 function App() {
   const [capturedImages, setCapturedImages] = useState([]);
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false);
+
+  const toggleMobileNav = () => {
+    setIsMobileNavOpen(!isMobileNavOpen);
+  }
 
   return (
     <div className="App">
@@ -18,6 +23,34 @@ function App() {
         <Link to="/">Home</Link>
         <Link to="/privacy-policy">Privacy Policy</Link>
         <Link to="/contact">Contact</Link>
+        <Link to="/" className="logo">Picapica</Link>
+
+        <div className={`hamburger ${isMobileNavOpen ? "open" : ""}`} onClick={toggleMobileNav}>
+           <div className= "bar"></div>
+           <div className= "bar"></div>
+           <div className= "bar"></div>
+        </div>
+        
+        <div className={`nav-links ${isMobileNavOpen ? "open" : ""}`}>
+          <Link to="/" onClick={toggleMobileNav}>Home</Link>
+          <Link to="/privacy-policy" onClick={toggleMobileNav}>Privacy Policy</Link>
+          <Link to="/contact" onClick={toggleMobileNav}>Contact</Link>
+
+          {/* PayPal Button */}
+          <form action="https://www.paypal.com/donate" method="post" target="_blank" className="paypal-form">
+            <a href="https://www.paypal.com/donate?hosted_button_id=VMLZHE6KGTZGQ"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Support me with a donation!">
+              <img
+                src="https://pics.paypal.com/00/s/YWRhODcwY2EtZWVhZC00OGY3LThhYTMtMzI1OWViYzIwYjUy/file.PNG"
+                alt="Donate with PayPal button"
+                className="paypal-button"
+              />
+            </a>
+          </form>
+        </div>
+
 
         <form action="https://www.paypal.com/donate" method="post" target="_blank" className="paypal-form">
          <a href="https://www.paypal.com/donate?hosted_button_id=VMLZHE6KGTZGQ" 
@@ -32,9 +65,7 @@ function App() {
               alt="Donate with PayPal button"
               className="paypal-button"
             />
-          </a>
-
-          
+          </a> 
         </form>
       </nav>
   
